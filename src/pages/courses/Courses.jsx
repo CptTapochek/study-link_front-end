8df48cdc5a3b93bd33e521e83be64be9db42b440
CptTapochek <andrei.bozu16@gmail.com>
2022-11-08 21:58:42 +0200

    course stage_1

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -5,7 +5,20 @@ const Courses = () => {
 
     return (
         <div className={style.main}>
-            Courses
+            <div className={style.title}>
+                <div className={style.faculty}>Faculty: ETTI</div>
+                <div className={style.semester}>I<sup>st</sup>semester</div>
+            </div>
+            <div className={style.coursesList}>
+                <a className={style.course}>
+                    <div className={style.courseTitle}><p>Electronica industriala 2</p></div>
+                    <div className={style.loading}>
+                        <div className={style.loadingBar}/>
+                    </div>
+                    <div className={style.count}>7/14</div>
+                </a>
+
+            </div>
         </div>
     );
 };
