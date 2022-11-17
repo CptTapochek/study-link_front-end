bb0f782065186907f7f5fa3ab24f4b1913e9d58e
CptTapo4ek <andreib@nextlogic.ro>
2022-11-17 23:58:38 +0200

    files list page

--- src/pages/files/Files.jsx
+++ src/pages/files/Files.jsx
@@ -5,7 +5,32 @@ const Files = () => {
 
     return (
         <div className={style.main}>
-            Files
+            <div className={style.title}>
+                <p>Saved Files</p>
+            </div>
+            <div className={style.infoBlock}>
+                <p className={style.fileName}>File Name</p>
+                <p className={style.course}>Course</p>
+                <p className={style.date}>Date</p>
+                <p className={style.action}>Action</p>
+            </div>
+            <div className={style.filesList}>
+                <div className={style.file}>
+                    <div className={style.fileTitle}>
+                        <i/><p>example_file_1.ext</p>
+                    </div>
+                    <div className={style.courseTitle}>Random title</div>
+                    <div className={style.fileDate}>13/11/2022</div>
+                    <div className={style.fileActions}>
+                        <div className={style.download}>
+                            <i/><p>Download</p>
+                        </div>
+                        <div className={style.delete}>
+                            <i/><p>Delete</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
