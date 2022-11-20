6f47bf5ae1a18e188db257a815bf87e2f8ff3856
CptTapo4ek <andreib@nextlogic.ro>
2022-11-20 02:15:22 +0200

    dashboard ui

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,7 +5,51 @@ const Dashboard = () => {
 
     return (
         <div className={style.main}>
-            Dashboard
+            <div className={style.title}>
+                <p>Dashboard</p>
+            </div>
+            <div className={style.row}>
+                <div className={`${style.card} ${style.studentsCard}`}>
+                    <div className={style.hexagonBack}><i/></div>
+                    <div className={style.cardTitle}>Total Students</div>
+                    <div className={style.cardValue}>1220</div>
+                </div>
+                <div className={`${style.card} ${style.teachersCard}`}>
+                    <div className={style.hexagonBack}><i/></div>
+                    <div className={style.cardTitle}>Total Teachers</div>
+                    <div className={style.cardValue}>120</div>
+                </div>
+                <div className={`${style.card} ${style.coursesCard}`}>
+                    <div className={style.hexagonBack}><i/></div>
+                    <div className={style.cardTitle}>Total Courses</div>
+                    <div className={style.cardValue}>12</div>
+                </div>
+                <div className={style.largeCard}>
+                    <div className={style.textSection}>
+                        <div className={style.largeCardTitle}>Courses Activities</div>
+                        <div className={style.largeCardLegend}>
+                            <p className={style.process}>Process</p>
+                            <p className={style.inProcess}>In Process</p>
+                        </div>
+                    </div>
+                    <div className={style.circularChart}>
+                        <div className={style.circleWrap}>
+                            <div className={style.circle}>
+                                <div className={`${style.mask} ${style.full}`}>
+                                    <div className={style.fill}/>
+                                </div>
+                                <div className={`${style.mask} ${style.half}`}>
+                                    <div className={style.fill}/>
+                                </div>
+                                <div className={style.insideCircle}>75%</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.row}>
+
+            </div>
         </div>
     );
 };
