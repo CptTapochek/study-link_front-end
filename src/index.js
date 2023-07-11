1d8caaf2050f602e8c50964d8085e78417243345
CptTapo4ek <andreib@nextlogic.ro>
2023-07-11 18:01:10 +0300

    redirect uri to https

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import {ApolloClient, ApolloProvider, gql, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://18.158.228.96:3000/api",
+    uri: "https://18.158.228.96:3000/api",
     cache: new InMemoryCache()
 });
 
